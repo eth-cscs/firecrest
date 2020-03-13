eb858e7e3a9758364362a56db036437f57ffb215
fcndl <fcndl@yahoo.com.ar>
2020-03-13 18:08:31 -0300

    fix upload tests for storage

--- src/tests/automated_tests/integration/test_storage.py
+++ src/tests/automated_tests/integration/test_storage.py
@@ -17,7 +17,7 @@ else:
 
 # same server used for utilities and external upload storage
 SERVER_UTILITIES_STORAGE = os.environ.get("SYSTEMS_PUBLIC").split(";")[0] 
-
+OBJECT_STORAGE = os.environ.get("OBJECT_STORAGE")
 
 def get_task(task_id, headers):
 	url = "{}/{}".format(TASKS_URL, task_id)
@@ -56,24 +56,44 @@ def test_post_upload_request(headers):
     # upload file to storage server
     msg = resp.json()["task"]["data"]["msg"]
     url = msg["url"]
-    params = [('AWSAccessKeyId', msg["AWSAccessKeyId"]), ('Signature', msg["Signature"]), ('Expires', msg["Expires"])]
 
-    # this way doesn't work
-    #files = {'file': ("testsbatch.sh", open(data["sourcePath"], 'rb'))}
-    #resp = requests.put(url=url, files=files)
-
-    # this is the only way signature doesn't break!
     resp = None
-    with open(data["sourcePath"], 'rb') as data:
-        resp= requests.put(url, data=data, params=params)
+    
+    if (OBJECT_STORAGE == "s3v2"):
+        params = [('AWSAccessKeyId', msg["AWSAccessKeyId"]), ('Signature', msg["Signature"]), ('Expires', msg["Expires"])]
+        
+        # this way doesn't work
+        # files = {'file': ("testsbatch.sh", open(data["sourcePath"], 'rb'))}
+        # resp = requests.put(url=url, files=files, params)
+        
+        # this is the only way signature doesn't break!
+        with open(data["sourcePath"], 'rb') as data:
+            resp= requests.put(url, data=data, params=params)
 
+    elif (OBJECT_STORAGE == "s3v4"):
+        post_data =  [('key', msg["key"]), ('policy', msg["policy"]), ('x-amz-algorithm', msg["x-amz-algorithm"])
+        , ('x-amz-credential', msg["x-amz-credential"]), ('x-amz-date', msg["x-amz-date"]),
+        ('x-amz-signature', msg["x-amz-signature"])]
+
+        files = {'file': open(data["sourcePath"],'rb')}
+        resp = requests.post(url, data=post_data, files=files)
+
+    else:
+        # swift post request
+        params = [('max_file_size', msg["max_file_size"]), ('max_file_count', msg["max_file_count"]), ('expires', msg["expires"]),
+        ('signature', msg["signature"]), ('redirect', msg["redirect"])]
+        
+        with open(data["sourcePath"], 'rb') as data:
+            resp= requests.put(url, data=data, params=params)
+            
     print(resp.content)
-    assert resp.status_code == 200
+    assert resp.status_code == 200 or resp.status_code == 204 #TODO: check 204 is right
 
     # inform upload finished
     headers.update({"X-Task-ID": task_id})
     r = requests.put(STORAGE_URL + "/xfer-external/upload", headers=headers)
     print(r.content)
+    
     assert r.status_code == 200
 
 
