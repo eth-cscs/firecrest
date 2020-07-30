bcec03abdd327aa3e3cf9cf07bedbfab8d6b0f9b
jpdorsch <jp.dorsch@gmail.com>
2020-07-30 16:13:54 +0200

    modified test for certificator to adapt this change

--- src/tests/automated_tests/unit/test_unit_certificator.py
+++ src/tests/automated_tests/unit/test_unit_certificator.py
@@ -10,27 +10,28 @@ if FIRECREST_URL:
 else:
     CERTIFICATOR_URL = os.environ.get("F7T_CERTIFICATOR_URL")	
 
-SERVER_COMPUTE = os.environ.get("F7T_SYSTEMS_INTERNAL_UTILITIES").split(";")[0]
+SYSTEM_NAME = os.environ.get("F7T_SYSTEMS_PUBLIC").split(";")[0]
+SYSTEM_ADDR = os.environ.get("F7T_SYSTEMS_INTERNAL_UTILITIES").split(";")[0]
 
 
-OPA_DATA = [("not_existing_system", 401), (SERVER_COMPUTE, 200)]
+OPA_DATA = [("not_existing_system", "not_existing_addr", 401), (SYSTEM_NAME, SYSTEM_ADDR, 200)]
 
 # Test get a certificate
 @host_environment_test
 def test_receive(headers):
 	# url = f"{CERTIFICATOR_URL}/?command=" + base64.urlsafe_b64encode("ls".encode()).decode()
 	params = {"command": base64.urlsafe_b64encode("ls".encode()).decode(),
-			  "cluster": SERVER_COMPUTE }
+			  "cluster": SYSTEM_NAME, "addr": SYSTEM_ADDR }
 	resp = requests.get(CERTIFICATOR_URL, headers=headers, params=params)
 	print(resp.content)
 	assert resp.status_code == 200
 
 @host_environment_test
-@pytest.mark.parametrize("machine, expected_response_code", OPA_DATA)
-def test_opa(machine,expected_response_code,headers):
+@pytest.mark.parametrize("machine, addr, expected_response_code", OPA_DATA)
+def test_opa(machine,addr,expected_response_code,headers):
 	# url = f"{CERTIFICATOR_URL}/?command=" + base64.urlsafe_b64encode("ls".encode()).decode()
 	params = {"command": base64.urlsafe_b64encode("ls".encode()).decode(),
-			  "cluster": machine }
+			  "cluster": machine, "addr": addr }
 	resp = requests.get(CERTIFICATOR_URL, headers=headers, params=params)
 	print(resp.content)
 	assert resp.status_code == expected_response_code
