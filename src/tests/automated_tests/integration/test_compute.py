ea6f71c87264c0faacd928b2b88ffac7ac773045
jpdorsch <jp.dorsch@gmail.com>
2020-11-13 18:21:43 +0100

    integration tests: added support for SSL

--- src/tests/automated_tests/integration/test_compute.py
+++ src/tests/automated_tests/integration/test_compute.py
@@ -15,21 +15,24 @@ else:
 JOBS_URL = COMPUTE_URL + "/jobs"
 SERVER_COMPUTE = os.environ.get("F7T_SYSTEMS_PUBLIC").split(";")[0]
 
-SSL_SIGNED = os.environ.get("F7T_SSL_SIGNED", False)
+### SSL parameters
+USE_SSL = os.environ.get("F7T_USE_SSL", False)
+SSL_CRT = os.environ.get("F7T_SSL_CRT", "")
+SSL_PATH = "../../../deploy/test-build"
 
 
 # Helper function for job submittings
 def submit_job(machine, headers, file='testsbatch.sh'):
 	files = {'file': ('upload.txt', open(file, 'rb'))}
 	headers.update({"X-Machine-Name": machine})
-	resp = requests.post(f"{JOBS_URL}/upload", headers=headers, files=files, verify=SSL_SIGNED)
+	resp = requests.post(f"{JOBS_URL}/upload", headers=headers, files=files, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
 	print(resp.content)
 	assert resp.status_code == 201
 	return resp
 
 def get_task(task_id, headers):
 	url = "{}/{}".format(TASKS_URL, task_id)
-	resp = requests.get(url, headers=headers, verify=SSL_SIGNED)
+	resp = requests.get(url, headers=headers, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
 	print(resp.content)
 	assert resp.status_code == 200
 	return resp
@@ -68,7 +71,7 @@ def test_submit_job(machine, headers):
 @pytest.mark.parametrize("machine", [SERVER_COMPUTE])
 def test_list_jobs(machine, headers):
 	headers.update({"X-Machine-Name": machine})
-	resp = requests.get(JOBS_URL, headers=headers, verify=SSL_SIGNED)
+	resp = requests.get(JOBS_URL, headers=headers, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
 	print(resp.content)
 	assert resp.status_code == 200
 	
@@ -81,7 +84,7 @@ def test_list_job(machine, headers):
 	jobid = -1
 	url = "{}/{}".format(JOBS_URL, jobid)
 	headers.update({"X-Machine-Name": machine})
-	resp = requests.get(url, headers=headers, verify=SSL_SIGNED)
+	resp = requests.get(url, headers=headers, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
 	print(resp.content)
 	assert resp.status_code == 400
 	# task_id = resp.json()["task_id"]
@@ -98,7 +101,7 @@ def test_cancel_job(machine, headers):
 	# cancel job
 	url = "{}/{}".format(JOBS_URL, job_id)
 	headers.update({"X-Machine-Name": machine})
-	resp = requests.delete(url, headers=headers, verify=SSL_SIGNED)
+	resp = requests.delete(url, headers=headers, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
 	print(resp.content)
 	assert resp.status_code == 200
 
@@ -118,7 +121,7 @@ def test_acct_job(machine, headers):
 	url = "{}/acct".format(COMPUTE_URL)
 	params = {"jobs": job_id}
 	headers.update({"X-Machine-Name": machine})
-	resp = requests.get(url, headers=headers,params=params, verify=SSL_SIGNED)
+	resp = requests.get(url, headers=headers,params=params, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
 	print(resp.content)
 	assert resp.status_code == 200
 
