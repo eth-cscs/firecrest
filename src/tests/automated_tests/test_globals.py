6b3b8424c28612863ad1041fce06543e0642b7d5
fcndl <fcndl@yahoo.com.ar>
2020-11-16 17:39:38 -0300

    fix demo environment tests

--- src/tests/automated_tests/test_globals.py
+++ src/tests/automated_tests/test_globals.py
@@ -4,10 +4,10 @@ import os
 #     will be TEST_USER for fake jwt, or service-account-{SA_CLIENT_ID} for sa login
 CURRENT_USER = ""
 
-if os.environ.get("SA_LOGIN", "").lower() != 'true':
-    CURRENT_USER = os.environ.get("TEST_USER")
+if os.environ.get("F7T_SA_LOGIN", "").lower() != 'true':
+    CURRENT_USER = os.environ.get("F7T_TEST_USER")
 else:
-    CURRENT_USER = 'service-account-' + os.environ.get("SA_CLIENT_ID")
+    CURRENT_USER = 'service-account-' + os.environ.get("F7T_SA_CLIENT_ID")
 
 USER_HOME = "/home/" + CURRENT_USER 
 
