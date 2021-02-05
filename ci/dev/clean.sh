8d3596db403d8f1ad64dcfbc9ec7daad3ccb8513
Tomas Aliaga <taliaga@cscs.ch>
2021-02-05 09:07:20 +0100

    Add missing ETH headers

--- ci/dev/clean.sh
+++ ci/dev/clean.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+##
+##  Copyright (c) 2019-2021, ETH Zurich. All rights reserved.
+##
+##  Please, refer to the LICENSE file in the root directory.
+##  SPDX-License-Identifier: BSD-3-Clause
+##
 set -euo pipefail
 
 echo "starting" $0
