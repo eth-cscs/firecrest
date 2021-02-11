89542baf9e5a2797cd15a1f4150b6dbe66526544
Tomas Aliaga <taliaga@cscs.ch>
2021-02-11 13:31:21 +0100

    Fixup jenkinsfile

--- ci/dev/test.sh
+++ ci/dev/test.sh
@@ -15,18 +15,20 @@ ${WORKSPACE}/ci/dev/clean.sh
 echo "starting containers..."
 docker-compose -f ${WORKSPACE}/deploy/test-build/docker-compose.yml up --build -d
 
-echo "waiting for Firecrest stack to be ready..."
-attempts=0
-while [[ "$attempts" -lt 9 && "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9000)" == "000" ]]; do
-    let "attempts+=1"
-    echo "API NOT ready, next attempt in 60 seconds"
-    sleep 60
-done
-if [[ "$attempts" -ge 9 ]]; then
-    echo "TIMEOUT waiting API. Shutting down cluster..."
-    docker-compose -f ${WORKSPACE}/deploy/test-build/docker-compose.yml down -v
-    exit 1
-fi
+# echo "waiting for Firecrest stack to be ready..."
+# attempts=0
+# while [[ "$attempts" -lt 9 && "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9000)" == "000" ]]; do
+#     let "attempts+=1"
+#     echo "API NOT ready, next attempt in 60 seconds"
+#     sleep 60
+# done
+# if [[ "$attempts" -ge 9 ]]; then
+#     echo "TIMEOUT waiting API. Shutting down cluster..."
+#     docker-compose -f ${WORKSPACE}/deploy/test-build/docker-compose.yml down -v
+#     exit 1
+# fi
+echo "sleeping..."
+sleep 120
 
 echo "running unit_tests..."
 docker run --rm -u $(id -u):$(id -g) -v ${WORKSPACE}:/firecrest --network f7t-frontend f7t-tester bash \
