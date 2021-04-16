44c6aba9f859ea13ca33acb0319240ae377668fd
aledabin <aledabin@gmail.com>
2021-04-16 19:26:27 +0200

    k8s: update script

--- deploy/k8s/apply_k8s.sh
+++ deploy/k8s/apply_k8s.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
+wait_running() {
+  echo " ... waiting for $1"
+  k1=$(kubectl get pods | grep ^deploy-$1 | grep Running)
+  while [ "$k1" == "" ]; do
+    sleep 1;
+    k1=$(kubectl get pods | grep ^deploy-$1 | grep Running)
+  done
+  pod=${k1%% *}
+}
+
+
 echo "* Deleting services..."
 kubectl delete all --all
+
+echo "* Killing port forwardings..."
+pkill -f "kubectl port-forward deploy-"
 
 echo
 echo "* Starting k8s..."
 kubectl apply -f . -R
 
-echo
-echo "* Forwarding ports for Keycloak and Kong (use 'jobs' to view)... "
+echo "* Doing port forwardings..."
+pod=""
+wait_running kong
+kubectl port-forward $pod 8000:8000 &> /dev/null &
+p="$!"
 
-k1=$(kubectl get pods | grep ^deploy-kong)
-while [ "$k1" == "" ]; do
-   sleep 1;
-   k1=$(kubectl get pods | grep ^deploy-kong)
-done
+wait_running keycloak
+kubectl port-forward $pod 8080:8080 &> /dev/null &
+p="$p $!"
 
-k1=${k1%% *}
-kubectl port-forward ${k1} 8000:8000 &> /dev/null &
-k1=$(kubectl get pods | grep ^deploy-keycloak)
-k1=${k1%% *}
-kubectl port-forward ${k1} 8080:8080 &> /dev/null &
-
-
+wait_running minio
+kubectl port-forward $pod 9000:9000 &> /dev/null &
+p="$p $!"
+echo "... all done, to kill forward processes: kill $p"
