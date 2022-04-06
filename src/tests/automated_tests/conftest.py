fc4a84342deadc4b813cea947932ba6d37bae3c9
adabin <alejandro.dabin@cscs.ch>
2022-04-06 17:08:41 +0200

    storage: fix test, remove unused vars

--- src/tests/automated_tests/conftest.py
+++ src/tests/automated_tests/conftest.py
@@ -31,22 +31,21 @@ SA_TOKEN_URI  = os.environ.get("F7T_SA_TOKEN_URI")
 SA_SECRET_KEY = os.environ.get("F7T_SA_SECRET_KEY")
 SA_CLIENT_ID  = os.environ.get("F7T_SA_CLIENT_ID")
 
+machine = os.environ.get("F7T_SYSTEMS_PUBLIC").strip('\'"').split(";")[0]
 
 @pytest.fixture(scope='session')
 def headers():
-    
+
     # authorization with fake jwt
     if SA_LOGIN.lower() != 'true':
         auth = "Bearer " + jwt.encode(payload, 'secret', algorithm='HS256').decode("utf-8")
-        return {"Authorization": auth, "Accept" : "application/json", "X-Firecrest-Service": "storage"}
+        return {"Authorization": auth, "Accept" : "application/json", "X-Firecrest-Service": "storage", "X-Machine-Name": machine}
 
     # authorization with sa account
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
     data = {"grant_type":"client_credentials"}
-    
+
     resp = requests.post(SA_TOKEN_URI, headers=headers, data=data, auth=(SA_CLIENT_ID, SA_SECRET_KEY))
     auth = resp.json()["token_type"] + " " + resp.json()["access_token"]
-    return {"Authorization": auth, "Accept" : "application/json", "X-Firecrest-Service": "storage"}
+    return {"Authorization": auth, "Accept" : "application/json", "X-Firecrest-Service": "storage", "X-Machine-Name": machine}
 
-   
-    
