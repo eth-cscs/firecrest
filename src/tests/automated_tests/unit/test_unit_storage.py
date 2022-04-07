8c0a68e2b85434dd464d050aa616f032701c7b6d
adabin <alejandro.dabin@cscs.ch>
2022-04-07 08:22:22 +0200

    test: storage: fix machine name

--- src/tests/automated_tests/unit/test_unit_storage.py
+++ src/tests/automated_tests/unit/test_unit_storage.py
@@ -14,8 +14,8 @@ FIRECREST_URL = os.environ.get("FIRECREST_URL")
 USE_GATEWAY  = (os.environ.get("USE_GATEWAY","false").lower() == "true")
 
 
-if FIRECREST_URL and USE_GATEWAY: 
-	STORAGE_URL = os.environ.get("FIRECREST_URL") + "/storage"    
+if FIRECREST_URL and USE_GATEWAY:
+    STORAGE_URL = os.environ.get("FIRECREST_URL") + "/storage"
 else:
     STORAGE_URL = os.environ.get("F7T_STORAGE_URL")
 
@@ -24,77 +24,80 @@ USE_SSL = os.environ.get("F7T_USE_SSL", False)
 SSL_CRT = os.environ.get("F7T_SSL_CRT", "")
 SSL_PATH = "../../../deploy/test-build"
 
+machine = os.environ.get("F7T_SYSTEMS_PUBLIC").strip('\'"').split(";")[0]
 
 # test upload request: ask for an upload task (must throw 200 OK)
 @skipif_not_uses_gateway
 def test_post_upload_request(headers):
+    headers["X-Machine-Name"] = machine
     data = { "sourcePath": "testsbatch.sh", "targetPath": USER_HOME }
     resp = requests.post(STORAGE_URL + "/xfer-external/upload", headers=headers, data=data, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
     assert resp.status_code == 201
 
 @skipif_not_uses_gateway
 def test_download_file_not_exist(headers):
+    headers["X-Machine-Name"] = machine
     data = { "sourcePath": "no-existing-file" }
-    resp = requests.post(STORAGE_URL + "/xfer-external/download", headers=headers, data=data, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False)) 
-    print(resp.json())  
+    resp = requests.post(STORAGE_URL + "/xfer-external/download", headers=headers, data=data, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
+    print(resp.json())
     print(resp.headers)
     assert resp.status_code == 400
 
 @skipif_not_uses_gateway
 def test_download_file_not_allowed(headers):
+    headers["X-Machine-Name"] = machine
     data = { "sourcePath": "/srv/f7t/test_sbatch_forbidden.sh" }
-    resp = requests.post(STORAGE_URL + "/xfer-external/download", headers=headers, data=data, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False)) 
-    print(resp.json())  
+    resp = requests.post(STORAGE_URL + "/xfer-external/download", headers=headers, data=data, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
+    print(resp.json())
     print(resp.headers)
     assert resp.status_code == 400
 
 @skipif_not_uses_gateway
 def test_download_dir_not_allowed(headers):
+    headers["X-Machine-Name"] = machine
     data = { "sourcePath": "/srv/f7t" }
-    resp = requests.post(STORAGE_URL + "/xfer-external/download", headers=headers, data=data, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False)) 
-    print(resp.json())  
+    resp = requests.post(STORAGE_URL + "/xfer-external/download", headers=headers, data=data, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
+    print(resp.json())
     print(resp.headers)
     assert resp.status_code == 400
 
 @skipif_not_uses_gateway
 def test_internal_cp(headers):
-    # jobName, time, stageOutJobId
+    headers["X-Machine-Name"] = machine
     data = {"sourcePath":  "/srv/f7t/test_sbatch.sh", "targetPath": USER_HOME + "/testsbatch2.sh", "account": "test"}
-    url = "{}/xfer-internal/cp".format(STORAGE_URL)
+    url = f"{STORAGE_URL}/xfer-internal/cp"
     resp = requests.post(url, headers=headers,data=data, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
     assert resp.status_code == 201
 
 @skipif_not_uses_gateway
-def test_internal_mv(headers):    
-    # jobName, time, stageOutJobId
+def test_internal_mv(headers):
+    headers["X-Machine-Name"] = machine
     data = {"sourcePath": "/srv/f7t/test_sbatch_mv.sh", "targetPath": USER_HOME + "/testsbatch3.sh"}
-    url = "{}/xfer-internal/mv".format(STORAGE_URL)
+    url = f"{STORAGE_URL}/xfer-internal/mv"
     resp = requests.post(url, headers=headers,data=data, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
     assert resp.status_code == 201
 
 @skipif_not_uses_gateway
 def test_internal_rsync(headers):
-    # jobName, time, stageOutJobId
+    headers["X-Machine-Name"] = machine
     data = {"sourcePath": USER_HOME + "/", "targetPath": USER_HOME + "/"}
-    url = "{}/xfer-internal/rsync".format(STORAGE_URL)
+    url = f"{STORAGE_URL}/xfer-internal/rsync"
     resp = requests.post(url, headers=headers,data=data, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
-    assert resp.status_code == 201   
+    assert resp.status_code == 201
 
 @skipif_not_uses_gateway
 def test_internal_rm(headers):
-    # jobName, time, stageOutJobId
+    headers["X-Machine-Name"] = machine
     data = {"targetPath": "/srv/f7t/test_sbatch_rm.sh"}
-    
-    url = "{}/xfer-internal/rm".format(STORAGE_URL)
+    url = f"{STORAGE_URL}/xfer-internal/rm"
     resp = requests.post(url, headers=headers,data=data, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
-    assert resp.status_code == 201   
+    assert resp.status_code == 201
 
 @skipif_not_uses_gateway
 def test_internal_rm_err(headers):
-    # jobName, time, stageOutJobId
+    headers["X-Machine-Name"] = machine
     data = {"targetPath": "/srv/f7t/test_sbatch_forbidden.sh"}
-    
-    url = "{}/xfer-internal/rm".format(STORAGE_URL)
+    url = f"{STORAGE_URL}/xfer-internal/rm"
     resp = requests.post(url, headers=headers,data=data, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
     assert resp.status_code == 400
 
@@ -102,9 +105,9 @@ def test_internal_rm_err(headers):
 # Test storage microservice status
 @skipif_uses_gateway
 def test_status():
-	url = "{}/status".format(STORAGE_URL)
-	resp = requests.get(url, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
-	assert resp.status_code == 200
+    url = f"{STORAGE_URL}/status"
+    resp = requests.get(url, verify= (f"{SSL_PATH}{SSL_CRT}" if USE_SSL else False))
+    assert resp.status_code == 200
 
 
 if __name__ == '__main__':
