14741bf344baf97cd8e542706679d4d85ce40084
Elia Palme <palmee@ethz.ch>
2024-02-14 09:43:50 +0100

    Fixed task deletion issue

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -355,7 +355,7 @@ def delete_task(id):
     try:
         global r
 
-        if not persistence.set_expire_task(r,tasks[hash_id],secs=300):
+        if not persistence.set_expire_task(r,tasks[hash_id].get_internal_status(),secs=300):
             return jsonify(error=f"Failed to delete task {hash_id} on persistence server"), 400
 
         data = jsonify(success=f"Task {hash_id} deleted")
@@ -417,7 +417,7 @@ def expire_task(id):
         global r
 
         app.logger.info(f"Set expiration for task {tasks[hash_id].task_id} - {exp_time} secs")
-        if not persistence.set_expire_task(r,tasks[hash_id],secs=exp_time):
+        if not persistence.set_expire_task(r,tasks[hash_id].get_internal_status(),secs=exp_time):
             app.logger.warning(f"Task couldn't be marked as expired")
             return jsonify(error="Failed to set expiration time on task in persistence server"), 400
 
