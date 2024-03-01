3c4a756b7c72e8c689ddb562d96dc955ce55ac11
jdorsch <jdorsch@cscs.ch>
2024-03-01 09:37:09 +0100

    adapted konk_config with new vars

--- deploy/demo/source/kong/update_kong_config.sh
+++ deploy/demo/source/kong/update_kong_config.sh
@@ -9,12 +9,26 @@
 # read common variables
 . ../../common/common.env
 
+F7T_HTTP_SCHEMA="http"
+
+if [ "${F7T_SSL_USE}" == "True" ]; then
+    F7T_HTTP_SCHEMA="https"; 
+fi
+
+echo "F7T_HTTP_SCHEMA: $F7T_HTTP_SCHEMA"
+
 # use '#' to separate string because '/' and ':' are valid on URLs
 sed -e 's#F7T_AUTH_TOKEN_ISSUER#'${F7T_AUTH_TOKEN_ISSUER}'#' \
     -e 's#F7T_REALM_RSA_PUBLIC_KEY#'${F7T_REALM_RSA_PUBLIC_KEY}'#' \
-    -e 's#F7T_KONG_COMPUTE_URL#'${F7T_KONG_COMPUTE_URL}'#' \
-    -e 's#F7T_KONG_STATUS_URL#'${F7T_KONG_STATUS_URL}'#' \
-    -e 's#F7T_KONG_STORAGE_URL#'${F7T_KONG_STORAGE_URL}'#' \
-    -e 's#F7T_KONG_TASKS_URL#'${F7T_KONG_TASKS_URL}'#' \
-    -e 's#F7T_KONG_UTILITIES_URL#'${F7T_KONG_UTILITIES_URL}'#' \
-       < kong.yml.template > ../../kong/kong.yml
+    -e 's#F7T_COMPUTE_HOST#'${F7T_COMPUTE_HOST}'#' \
+    -e 's#F7T_COMPUTE_PORT#'${F7T_COMPUTE_PORT}'#' \
+    -e 's#F7T_STATUS_HOST#'${F7T_STATUS_HOST}'#' \
+    -e 's#F7T_STATUS_PORT#'${F7T_STATUS_PORT}'#' \
+    -e 's#F7T_STORAGE_HOST#'${F7T_STORAGE_HOST}'#' \
+    -e 's#F7T_STORAGE_PORT#'${F7T_STORAGE_PORT}'#' \
+    -e 's#F7T_TASKS_HOST#'${F7T_TASKS_HOST}'#' \
+    -e 's#F7T_TASKS_PORT#'${F7T_TASKS_PORT}'#' \
+    -e 's#F7T_UTILITIES_HOST#'${F7T_UTILITIES_HOST}'#' \
+    -e 's#F7T_UTILITIES_PORT#'${F7T_UTILITIES_PORT}'#' \
+    -e 's#F7T_HTTP_SCHEMA#'${F7T_HTTP_SCHEMA}'#' \
+       < kong.yml.template > ../../kong/kong.yml 
