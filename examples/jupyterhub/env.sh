7c7c2c3e371886a0b43ed99cd28577217b679f57
Rafael Sarmiento <rafael.sarmiento@cscs.ch>
2024-11-20 12:27:26 +0100

    remove hardcoded path

--- examples/jupyterhub/env.sh
+++ examples/jupyterhub/env.sh
@@ -2,3 +2,5 @@ export FIRECREST_URL=http://localhost:8000
 export SA_CLIENT_ID=firecrest-sample
 export SA_CLIENT_SECRET=b391e177-fa50-4987-beaf-e6d33ca93571
 export SA_AUTH_TOKEN_URL=http://localhost:8080/auth/realms/kcrealm/protocol/openid-connect/token
+
+export JHUB_DOCKERFILE_DIR=$PWD
